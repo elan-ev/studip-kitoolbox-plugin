d16d2e752cdb549164cc22036020c460bd38da0c
Ron Lucke <lucke@elan-ev.de>
2024-07-08 12:50:54 +0200

    remove unused css

--- src/components/course/ToolItem.vue
+++ src/components/course/ToolItem.vue
@@ -143,14 +143,5 @@ const showJWTLink = computed(() => {
             max-width: 540px;
         }
     }
-
-    .kit-tool-item-anchor {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        z-index: 100;
-    }
 }
 </style>
